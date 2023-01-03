a93ae56b085f3977ef01032de9457a7bcba2cc0c
netapy <39877685+netapy@users.noreply.github.com>
2023-01-03 09:41:03 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Footer from '../components/footer.svelte';
+</script>
+
+<div class="d-flex align-items-center justify-content-between flex-column" style="height:100dvh">
+	<div />
+	<slot />
+	<Footer />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script></script>
+
+<div>
+    <div class="d-flex align-items-center justify-contente-center flex-column" style="height: auto;">
+        <div class="bigTitle">METANOIA</div>
+        <div class="d-flex align-items-center justify-content-center p-3">
+            <input class="mailInput" type="text" placeholder="bruce@wayne.com" />
+            <button class="transparentButton hvr-grow px-3">
+                <img src="/send.svg" alt="send icon">
+            </button>
+        </div>
+    </div>
+</div>
+
+<style>
+	.bigTitle {
+		font-size: clamp(5vw, 7rem, 16vw);
+        font-weight: 700;
+        font-family: 'Nunito', sans-serif;
+	}
+
+	.mailInput {
+		background: #ffffff;
+		border: 1.5px solid #c8c6c6;
+		border-radius: 14px;
+        padding: 10px 10px;
+        font-size: 1.1rem;
+        max-width: 50vw;
+        width: 300px;
+	}
+
+    .transparentButton{
+        background: none;
+        border:none;
+    }
+</style>
